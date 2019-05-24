f387f63ed49bffaac7ce4a0a4002c9cb4d1d79c7
Mantas Naris <man61@case.edu>
2019-05-24 17:23:08 -0400

    this code won't work

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,32 @@
 //uncomment if running with serial output "debugging"
 //#define SERIAL_DEBUGGING 1
 
+
+//variables for the timer
+
 //time elapsed (in miliseconds)
 int time_elapsed = 0;
 //timer interval (in miliseconds)
 int time_interval = 500;
 //the timer object
 SimpleTimer timer;
+
+
+//variables for the SD card
+
+//SD card chip select pin
+const int chip_select = 10;//for adafruit feather
+String directory_name = "yop.txt";
+String file_name = "yop";
+
+//std::string str = "string";
+
+const char *cstr = directory_name.c_str();
+//https://stackoverflow.com/questions/7352099/stdstring-to-char/7352131
+//https://stackoverflow.com/questions/347949/how-to-convert-a-stdstring-to-const-char-or-char
+
+File active_file;
+
 
 //Clay is a fuckin moron
 int super_important = 0;
@@ -27,6 +47,8 @@ U8X8_SSD1306_128X32_UNIVISION_HW_I2C u8x8(U8X8_PIN_NONE);
 // callback vs interrupt
 // pointers * vs &
 
+
+
 void setup() {
     // put your setup code here, to run once:
     u8x8.begin();
@@ -35,6 +57,17 @@ void setup() {
     u8x8.clearDisplay();
     u8x8.println("booting...");
     //u8x8.draw2x2String(0,0,packetBuffer);
+
+
+    //SD.begin(chip_select);
+    if (!SD.begin(chip_select)) {
+        u8x8.println("initialization failed!");
+    }
+
+
+    active_file = SD.open(cstr, FILE_WRITE);
+    active_file.println("test");
+    active_file.close();
 }
 
 void loop() {
